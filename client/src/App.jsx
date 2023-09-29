a2b097e5df3668c22340dd335f03b82f74667cee
Arthur <arthurzinugf@gmail.com>
2023-09-28 22:18:02 -0300

    feat(new-dependeci): add and configure tailwindCss

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,7 @@
-import "./App.css";
 import { useEffect, useState } from "react";
 import { Outlet, Link } from "react-router-dom";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import { HomeForms } from './components/Form/HomeForms'
 // Importing components
 import Tweets from "./components/Tweets";
 import NewTweet from './components/NewTweet'
@@ -12,14 +11,19 @@ import EditTweet from './components/EditTweet'
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/tweets" element={<Tweets />} />
-        <Route path="/tweets/new" element={<NewTweet />} />
-        <Route path="/tweets/:id/edit" element={<EditTweet />} />
-        <Route path="/tweets/:id" element={<TweetDetails />} />
-      </Routes>
-    </Router>
+    <div className="flex justify-center items-center h-screen">
+      <main className="p-12 bg-white	 rounded-3xl	">
+      <HomeForms></HomeForms>
+        <Router>
+          <Routes>
+            <Route path="/tweets" element={<Tweets />} />
+            <Route path="/tweets/new" element={<NewTweet />} />
+            <Route path="/tweets/:id/edit" element={<EditTweet />} />
+            <Route path="/tweets/:id" element={<TweetDetails />} />
+          </Routes>
+      </Router>
+      </main>
+    </div>
   );
 }
 
